585d213a4097ef8b58e25ab96782855bdfaa28de
Sebastiaan <159073521+FishMansss@users.noreply.github.com>
2024-12-12 11:47:51 -0800

    e

---
+++ mapGenerator.py
@@ -0,0 +1,58 @@
+import random, os
+os.system('cls')
+
+def dice(numberOfDie):
+    x = random.randint(numberOfDie,(6 * numberOfDie))
+    return x
+
+world = {}
+for i in range(1):
+##starport
+    x = dice(2)
+    if x ==1 or x ==2 or x ==3:
+        sp = 'A'
+    if x ==4 or x ==5:
+        sp = 'B'
+    if x  in [7,8]:
+        sp = 'C'
+    if x ==9:
+        sp = 'D'
+    if x ==10 or x ==11:
+        sp = 'E'
+    if x ==12:
+        sp = 'X'
+    world.update(starport=sp)
+
+    ##check for naval base
+    x = dice(2)
+    if x<7:
+        navalBase=False
+    if x>6:
+        navalBase=True
+    world.update(NavalBase=navalBase)
+
+    ##check for scout base
+    x = dice(2)
+    if x<7:
+        scoutBase=False
+    if x>6:
+        scoutBase=True
+    world.update(ScoutBase=scoutBase)
+
+    ##check for gas giant
+    x = dice(2)
+    if x<10:
+        gasGiant=False
+    if x>6:
+        gasGiant=True
+    world.update(GasGiant=gasGiant)
+
+    ##Generate random name
+    x = dice(100)
+    name = "lV" + str(x)
+
+
+    world.update(Name=name)
+    print(world)
+    
+    
